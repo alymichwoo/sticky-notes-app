4c43e245e440bf5893eb427e4676680e0d7d6954
Alyssa Woods <alyssawoods3@gmail.com>
2023-09-27 18:35:23 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import Header from "./Header";
+import NotesList from "./NotesList";
+
+class App extends Component {
+  state = {
+    notes: [
+      {
+        id: Date.now(),
+        title: "",
+        description: "",
+        doesMatchSearch: true
+      }
+    ],
+    searchText: ""
+  };
+
+  addNote = (note) => {
+    // create a new note
+    const newNote = {
+      id: Date.now(),
+      title: "",
+      description: "",
+      doesMatchSearch: true
+    };
+    // add new note to existing notes array in State
+    this.setState({ notes: [newNote, ...this.state.notes] });
+  };
+
+  removeNote = (noteId) => {
+    const updatedNotes = this.state.notes.filter((note) => note.id !== noteId);
+    this.setState({ notes: updatedNotes });
+  };
+
+  onType = (editMeId, updatedKey, updatedValue) => {
+    // editMeId == id of note that is edited
+    // updatedKey == title or description field
+    // updatedValue == value of title or description
+
+    const updatedNotes = this.state.notes.map((note) => {
+      if (note.id !== editMeId) {
+        return note;
+      } else {
+        if (updatedKey === "title") {
+          note.title = updatedValue;
+          return note;
+        } else {
+          note.description = updatedValue;
+          return note;
+        }
+      }
+    });
+    this.setState({ notes: updatedNotes });
+  };
+
+  onSearch = (text) => {
+    const newSearchText = text.toLowerCase();
+
+    const searchedNotes = this.state.notes.map((note) => {
+      if (!newSearchText) {
+        note.doesMatchSearch = true;
+        return note;
+      } else {
+        const title = note.title.toLowerCase();
+        const description = note.description.toLowerCase();
+        const titleMatch = title.includes(newSearchText);
+        const descriptionMatch = description.includes(newSearchText);
+        const hasMatch = titleMatch || descriptionMatch;
+        note.doesMatchSearch = hasMatch;
+        return note;
+      }
+    });
+    this.setState({ notes: searchedNotes, searchText: newSearchText });
+  };
+
+  componentDidUpdate() {
+    const savedNotesString = JSON.stringify(this.state.notes);
+    localStorage.setItem("savedNotes", savedNotesString);
+  }
+
+  componentDidMount() {
+    const savedNotesString = localStorage.getItem("savedNotes");
+    if (savedNotesString) {
+      const savedNotes = JSON.parse(savedNotesString);
+      this.setState({ notes: savedNotes });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Header
+          addNote={this.addNote}
+          searchText={this.state.searchText}
+          onSearch={this.onSearch}
+        />
+        <NotesList
+          notes={this.state.notes}
+          onType={this.onType}
+          removeNote={this.removeNote}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
